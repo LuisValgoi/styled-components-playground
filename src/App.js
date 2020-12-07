fea35a25c05153375d75558f3c55d695f561e2c5
Luis Valgoi <luis.valgoi@sap.com>
2020-12-07 11:13:08 -0300

    refactor: added span

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { HeaderRegular, HeaderSmall } from "./styles/Header";
 const App = () => {
   return (
     <>
-      <HeaderRegular fontSize={60}>Regular Text</HeaderRegular>
+      <HeaderRegular fontSize={60}>
+        Regular Text<span>span inside of it</span>
+      </HeaderRegular>
       <HeaderSmall>Smaller Text</HeaderSmall>
     </>
   );
